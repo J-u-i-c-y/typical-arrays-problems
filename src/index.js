41f8ca78cef65eef6be61650759ad65c340095a5
Andrey Goncharenko <goncharenko.andry@yandex.by>
2020-10-03 19:55:18 +0300

    feat: solving problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined) return 0;
+  let value = array[0];
+  if(array.length === 0) {
+    return 0;
+  } 
+  for(let i = 0; i < array.length; i++) {
+    if(value > array[i]){
+      value = array[i];
+    }
+  }
+  return value;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined) return 0;
+  let value = array[0];
+  if(array.length === 0) {
+    return 0;
+  } 
+  for(let i = 0; i < array.length; i++) {
+    if(value < array[i]){
+      value = array[i];
+    }
+  }
+  return value;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length === 0) {
+    return 0;
+  } 
+  let count = 0;
+  for(let i = 0; i < array.length; i++) {
+    count += array[i];
+  }
+  return count / array.length;;
 }
